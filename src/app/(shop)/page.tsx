db4324ee8ca3345db439ea0c0a3cba12c32b0115
Juan David Sanguino <juandavidsanguinoo@gmail.com>
2023-12-26 11:12:12 -0500

    Refactor layout component and remove unused page component

---
+++ src/app/(shop)/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Shop Page</h1>
+    </main>
+  );
+}
---
+++ src/app/auth/new-account/page.tsx
@@ -0,0 +1,7 @@
+export default function NewAccount() {
+  return (
+    <main>
+      <h1>Register Page</h1>
+    </main>
+  );
+}
