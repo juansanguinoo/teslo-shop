b92b5f996fd448d4cef7c610c6afd9ce97823aa1
Juan David Sanguino <juandavidsanguinoo@gmail.com>
2023-12-26 21:51:58 -0500

    Add Sidebar component to ShopLayout

--- src/app/(shop)/layout.tsx
+++ src/app/(shop)/layout.tsx
@@ -1,4 +1,4 @@
-import { TopMenu } from "@/components";
+import { Sidebar, TopMenu } from "@/components";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -14,6 +14,7 @@ export default function ShopLayout({
   return (
     <main className="min-h-screen">
       <TopMenu />
+      <Sidebar />
       <div className="px-0 sm:px-10">{children}</div>
     </main>
   );
