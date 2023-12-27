0b201daf876a58ea096f90f23ef41ddd01ba4af2
Juan David Sanguino <juandavidsanguinoo@gmail.com>
2023-12-27 16:27:22 -0500

    Add checkout page layout and functionality

--- src/app/(shop)/checkout/page.tsx
+++ src/app/(shop)/checkout/page.tsx
@@ -1,7 +1,99 @@
+import { Title } from "@/components";
+import { initialData } from "@/seed/seed";
+import Image from "next/image";
+import Link from "next/link";
+
+const productsInCart = [
+  initialData.products[0],
+  initialData.products[1],
+  initialData.products[2],
+];
+
 export default function CheckoutPage() {
   return (
-    <div>
-      <h1>Checkout Page</h1>
+    <div className="flex justify-center items-center mb-72 px-10 sm:px-0">
+      <div className="flex flex-col w-[1000px]">
+        <Title title="Verificar orden" />
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-10">
+          <div className="flex flex-col mt-5">
+            <span className="text-xl">Ajustar elementos</span>
+            <Link href="/cart" className="underline mb-5">
+              Editar carrito
+            </Link>
+
+            {productsInCart.map((product) => (
+              <div className="flex mb-5" key={product.slug}>
+                <Image
+                  src={`/products/${product.images[0]}`}
+                  alt={product.title}
+                  width={100}
+                  height={100}
+                  style={{
+                    width: "100px",
+                    height: "100px",
+                  }}
+                  className="mr-5 rounded"
+                />
+
+                <div className="">
+                  <p>{product.title}</p>
+                  <p>{product.price} x 3</p>
+                  <p className="font-bold">Subtotal: ${product.price * 3}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+
+          <div className="bag-wihte rounded-xl shadow-xl p-7">
+            <h2 className="text-2xl mb-2">Dirección de entrega</h2>
+
+            <div className="mb-10">
+              <p className="text-xl">Juan David Sanguino</p>
+              <p>Av. Siempre Viva</p>
+              <p>Col. Centro</p>
+              <p>Alcaldía Cuauhtémoc</p>
+              <p>Ciudad de México</p>
+              <p>CP 123123123</p>
+              <p>123 123 1234</p>
+            </div>
+
+            <div className="w-full h-0.5 rounded bg-gray-200 mb-10"></div>
+
+            <h2 className="text-2xl mb-2">Resumén de ordén</h2>
+
+            <div className="grid grid-cols-2">
+              <span>No. de productos</span>
+              <span className="text-right">3 Artículos</span>
+
+              <span>Subtotal</span>
+              <span className="text-right">$ 100</span>
+
+              <span>Impuestos (15%)</span>
+              <span className="text-right">$ 100</span>
+
+              <span className="text-2xl mt-5">Total:</span>
+              <span className="mt-5 text-2xl text-right">$ 100</span>
+            </div>
+
+            <div className="mt-5 mb-2 w-full">
+              <span className="text-xs">
+                Al confirmar tu orden aceptas los{" "}
+                <Link href="/terms" className="underline">
+                  términos y condiciones
+                </Link>
+              </span>
+
+              <Link
+                href="/orders/123"
+                className="flex btn-primary justify-center"
+              >
+                Confirmar orden
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
