ac882c3c2c35f93c363d9cb1edb694d5d4d0fdab
Juan David Sanguino <juandavidsanguinoo@gmail.com>
2024-01-08 19:05:48 -0500

    Add bcryptjs dependency and seed user data

--- src/seed/seed.ts
+++ src/seed/seed.ts
@@ -1,3 +1,5 @@
+import bcryptjs from "bcryptjs";
+
 interface SeedProduct {
   description: string;
   images: string[];
@@ -11,15 +13,38 @@ interface SeedProduct {
   gender: "men" | "women" | "kid" | "unisex";
 }
 
+interface SeedUser {
+  email: string;
+  password: string;
+  name: string;
+  role: "admin" | "user";
+}
+
 type ValidSizes = "XS" | "S" | "M" | "L" | "XL" | "XXL" | "XXXL";
 type ValidTypes = "shirts" | "pants" | "hoodies" | "hats";
 
 interface SeedData {
+  users: SeedUser[];
   categories: string[];
   products: SeedProduct[];
 }
 
 export const initialData: SeedData = {
+  users: [
+    {
+      email: "juandavid@google.com",
+      name: "Juan David",
+      password: bcryptjs.hashSync("123456"),
+      role: "admin",
+    },
+    {
+      email: "edinael@google.com",
+      name: "Edinael Sanguino",
+      password: bcryptjs.hashSync("123456"),
+      role: "user",
+    },
+  ],
+
   categories: ["Shirts", "Pants", "Hoodies", "Hats"],
 
   products: [
